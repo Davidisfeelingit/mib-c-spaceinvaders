33d8f7571a2c0b624f592f1f9142f932c5a35bf1
unknown <jan.liebeck@stud.htwk-leipzig.de>
2020-12-01 17:44:24 +0100

    Added Player/Character

--- src/mib/c/game/Character.java
+++ src/mib/c/game/Character.java
@@ -1,4 +1,20 @@
 package mib.c.game;
 
-public class Character {
+public abstract class Character {
+    /**
+     * Function which manages the player moves.
+     */
+    public void Move(int direction){
+
+    }
+
+    /**
+     * Function which draws the character build.
+     * Uses parameters for graphics.
+     * parameters will be added, if graphic objects are added.
+     */
+    public void Draw(){
+
+    }
 }
+
--- src/mib/c/game/Player.java
+++ src/mib/c/game/Player.java
@@ -1,4 +1,42 @@
 package mib.c.game;
 
-public class Player {
+
+public class Player extends Character {
+    int position_x;
+    int position_y;
+    boolean alive;
+    //TODO: implement missing imports
+    /*
+    rectangle collisionArea;
+    bitmap image;
+    */
+    /**
+     * Constructor ...
+     */
+    public void Player(){
+
+    }
+
+    /**
+     * Function which will move the player.
+     */
+    public void Move(){
+
+    }
+
+    /**
+     * Function which draws the player build.
+     */
+    public void Draw(){
+
+    }
+
+    /**
+     * Function which updates the current player.
+     */
+    public void loadPlayer(){
+
+    }
+
+
 }
