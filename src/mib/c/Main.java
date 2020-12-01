4cf3c96913101fed91ce43149b128ff982bdc6ee
Lucas Kohstall <reiermeyer@web.de>
2020-12-01 17:15:02 +0100

    Added project.

---
+++ src/mib/c/Main.java
@@ -0,0 +1,8 @@
+package mib.c;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/mib/c/game/Shot.java
@@ -0,0 +1,4 @@
+package mib.c.game;
+
+public class Shot {
+}
