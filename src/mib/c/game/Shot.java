bd4f7d78427640f12d2356d2e8c8f94cbd79bf5a
David Deutschmann <david.deutschmann@stud.htwk-leipzig.de>
2020-12-14 16:07:32 +0100

    added Javadoc to Shot class

--- src/mib/c/game/Shot.java
+++ src/mib/c/game/Shot.java
@@ -3,13 +3,28 @@ package mib.c.game;
 import java.awt.*;
 
 public class Shot {
-    Point location;
-    Rectangle ShotSize;
+    /**
+     * current Shot's location
+     */
+    public Point location;
 
+    /**
+     * Collision Area of shot
+     */
+    public Rectangle ShotSize;
+
+    /**
+     * initializes a new Shot with taken location
+     * @param there defines the spawning location
+     */
     public void Shot(Point there) {
         //do something good
     }
 
+    /**
+     * draws graphics on screen
+     * @param graphics
+     */
     public void Draw(Graphics graphics){
         //draw dem graphics
     }
