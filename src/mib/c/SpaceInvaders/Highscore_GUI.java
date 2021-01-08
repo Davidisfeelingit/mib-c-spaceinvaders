9e2459fb51c5be9bdf8fe254b5c7ae095f1ce480
David Deutschmann <david.deutschmann@stud.htwk-leipzig.de>
2021-01-08 15:21:00 +0100

    getHighscore now functional

--- src/mib/c/SpaceInvaders/Highscore_GUI.java
+++ src/mib/c/SpaceInvaders/Highscore_GUI.java
@@ -4,6 +4,8 @@ import java.awt.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Highscore_GUI extends JFrame {
@@ -16,12 +18,7 @@ public class Highscore_GUI extends JFrame {
 
     public Highscore_GUI(){
         String[] columnNames = {"Name","Score"};
-        Object[][]currentHighscore = {
-                {"Lucas","1000"},
-                {"David","2000"},
-                {"Jan","3000"}
-        };
-        highscoreT = new JTable(currentHighscore, columnNames);
+        highscoreT = new JTable(getHighscore(), columnNames);
         this.highscore_GUI = highscore_GUI;
         frame1.setSize(800,600);
         Container mainP = frame1.getContentPane();
@@ -45,20 +42,29 @@ public class Highscore_GUI extends JFrame {
     {
         getHighscore();
     }
-    public static String getHighscore(){
+    public static String[][] getHighscore(){
         FileReader readFile = null;
         BufferedReader reader = null;
-        try{
+        String[][] highscore = new String[10][2];
+        try {
             readFile = new FileReader("highscore.txt");
             reader = new BufferedReader(readFile);
-            //System.out.println(reader.readLine());
-            return reader.readLine();
+            String tempHigh = reader.readLine();
+            int counter = 0;
+            while (tempHigh != null) {
+                highscore[counter][0] = tempHigh;
+                highscore[counter][1] = reader.readLine();;
+                tempHigh = reader.readLine();
+                counter++;
+            }
+            return highscore;
+        }
+        catch (Exception e){
+            return null;
+        }
 
-        }
-        catch (Exception e)
-        {
-            return "0";
-        }
+
+
         finally{
             try{
                 reader.close();
