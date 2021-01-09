26d6f20ccda88f3248f30c2dc494ff9412bb9689
David Deutschmann <david.deutschmann@stud.htwk-leipzig.de>
2021-01-09 19:35:45 +0100

    added sorting to highscore saving and changed saving behaviour
    Before saving, the whole json gets resorted together with the new entry and then overwritten with the new content, \nadditionally changed the return of readHighscore() to return arrays of length=10 to be easier usable afterwords,\n that way in the game only 10 highscores get displayed, but every highscore gets saved in the json file

--- src/mib/c/SpaceInvaders/Highscore.java
+++ src/mib/c/SpaceInvaders/Highscore.java
@@ -6,6 +6,7 @@ import org.json.JSONObject;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.*;
 
 public class Highscore {
 
@@ -20,8 +21,8 @@ public class Highscore {
             JSONArray highscore = object.getJSONArray("highscore");
 
             // Convert to 2d array.
-            String[][] highscoreTable = new String[highscore.length()][2];
-            for (int i = 0; i < highscore.length(); i++) {
+            String[][] highscoreTable = new String[10][2];
+            for (int i = 0; i < 10; i++) {
                 String score = highscore.getString(i);
                 String[] splits = score.split(":");
 
@@ -45,9 +46,48 @@ public class Highscore {
             // Convert to json array.
             JSONObject object = new JSONObject(content);
             object.getJSONArray("highscore").put(newHighScore);
+            JSONArray highscore = object.getJSONArray("highscore");
+
+            // Convert to 2d List.
+            ArrayList<ArrayList <String>> highscores = new ArrayList<ArrayList<String>>();
+            String[][] highscoreTable = new String[highscore.length()][2];
+            for (int i = 0; i < highscore.length(); i++) {
+                String score = highscore.getString(i);
+                String[] splits = score.split(":");
+
+                ArrayList<String> temp = new ArrayList<String>();
+                temp.add(splits[1]);
+                temp.add(splits[0]);
+                highscores.add(temp);
+            }
+
+            //sort JSON  by overwritten comparefunction
+            Collections.sort(highscores, new Comparator<ArrayList<String>>() {
+                @Override
+                public int compare(ArrayList<String> o1, ArrayList<String> o2) {
+                    int s1 = Integer.parseInt(o1.get(0));
+                    int s2 = Integer.parseInt(o2.get(0));
+                    int compare = (s1 > s2) ? 1 : 0;
+                    if(compare == 0){
+                        compare = (s1 == s2) ? 0 : -1;
+                    }
+                    return compare;
+                }
+            });
+            Collections.reverse(highscores);
+
+            // Convert to json object.
+            String newContent = "{\"highscore\":[";
+            for (int i=0; i < highscores.size(); i++) {
+
+                newContent = newContent + "\"" + highscores.get(i).get(1) + ":" + highscores.get(i).get(0) + "\",";
+            }
+            JSONObject newObject = new JSONObject(newContent + "]}");
+
+
 
             // Write to file
-            Files.write(Path.of("highscore.json"), object.toString().getBytes(StandardCharsets.UTF_8));
+            Files.write(Path.of("highscore.json"), newObject.toString().getBytes(StandardCharsets.UTF_8));
         } catch (Exception e) {
             System.out.print(e);
         }
