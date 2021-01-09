0c86540865b44099c140881d95c7cd4eee2c33e0
Lucas <lucaskohstall@web.de>
2021-01-09 14:27:30 +0100

    Cleanup

--- src/mib/c/SpaceInvaders/Menu_GUI.java
+++ src/mib/c/SpaceInvaders/Menu_GUI.java
@@ -1,89 +1,74 @@
 package mib.c.SpaceInvaders;
 
-import jdk.jfr.Event;
-
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
-import java.io.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Menu_GUI extends JPanel implements ActionListener {
-    private JLabel titleL, highscoreL, nameL;
-    private JButton startB, highscoreB, exitB;
-    private JTable highscoreT;
-    private JLabel currentNumber;
-    private String[] namesHighscore = {"highscore.txt"};
-    private int currentIndex = 0;
-    private String[] temp1Array = new String[2];
-
-
 
     public Menu_GUI() throws FileNotFoundException {
         setSize(800, 600);
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        titleL = new JLabel("SpAcE InVaDeRs");
-        titleL.setFont(new Font("Chiller", Font.BOLD, 50));
-        titleL.setAlignmentX(Component.CENTER_ALIGNMENT);
-        //titleL.setBounds(100, 20, 400, 50);
-        add(titleL);
-
+        JLabel titleLabel = new JLabel("SpAcE InVaDeRs");
+        titleLabel.setFont(new Font("Chiller", Font.BOLD, 50));
+        titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        add(titleLabel);
 
         JPanel buttonPanel = new JPanel(null);
 
         // Start button.
-        startB = new JButton(new ImageIcon(("src/images/start.png")));
-        startB.addActionListener((e) -> startGame());
-        startB.setOpaque(false);
-        startB.setContentAreaFilled(true);
-        startB.setBorderPainted(true);
-        startB.setBounds(310, 80, 180, 29);
-        startB.setPreferredSize(new Dimension(180, 29));
-        startB.setOpaque(false);
-        startB.setAlignmentX(Component.CENTER_ALIGNMENT);
-        buttonPanel.add(startB);
+        JButton startButton = new JButton(new ImageIcon(("src/images/start.png")));
+        startButton.addActionListener((e) -> startGame());
+        startButton.setOpaque(false);
+        startButton.setContentAreaFilled(true);
+        startButton.setBorderPainted(true);
+        startButton.setBounds(310, 80, 180, 29);
+        startButton.setPreferredSize(new Dimension(180, 29));
+        startButton.setOpaque(false);
+        buttonPanel.add(startButton);
+        startButton.addActionListener(this);
 
         // Highscore button.
-        highscoreB = new JButton(new ImageIcon("src/images/highscore.png"));
-        highscoreB.setOpaque(false);
-        highscoreB.setContentAreaFilled(true);
-        highscoreB.setBorderPainted(true);
-        highscoreB.setBounds(300, 135, 200, 26);
-        highscoreB.setPreferredSize(new Dimension(200, 26));
-        /*highscoreB.setAlignmentX(Component.CENTER_ALIGNMENT);
-        highscoreB.setAlignmentY(Component.CENTER_ALIGNMENT);*/
-        highscoreB.addActionListener(e -> {
-            try {
-                readFile("highscore.txt");
-                Highscore_GUI highscore_gui = new Highscore_GUI();
-            } catch (FileNotFoundException fileNotFoundException) {
-                fileNotFoundException.printStackTrace();
-            }
+        JButton highscoreButton = new JButton(new ImageIcon("src/images/highscore.png"));
+        highscoreButton.setOpaque(false);
+        highscoreButton.setContentAreaFilled(true);
+        highscoreButton.setBorderPainted(true);
+        highscoreButton.setBounds(300, 135, 200, 26);
+        highscoreButton.setPreferredSize(new Dimension(200, 26));
+        highscoreButton.addActionListener(e -> {
+            Highscore_GUI highscore_gui = new Highscore_GUI();
         });
-        buttonPanel.add(highscoreB);
+        highscoreButton.addActionListener(this);
+        buttonPanel.add(highscoreButton);
 
         // Exit button.
-        exitB = new JButton(new ImageIcon(("src/images/exit.png")));
-        exitB.setOpaque(false);
-        exitB.setContentAreaFilled(true);
-        exitB.setBorderPainted(true);
-        exitB.setBounds(340, 190, 120, 31);
-        exitB.setPreferredSize(new Dimension(120, 31));
-        exitB.setAlignmentX(Component.CENTER_ALIGNMENT);
-        exitB.addActionListener(e -> {
+        JButton exitButton = new JButton(new ImageIcon(("src/images/exit.png")));
+        exitButton.setOpaque(false);
+        exitButton.setContentAreaFilled(true);
+        exitButton.setBorderPainted(true);
+        exitButton.setBounds(340, 190, 120, 31);
+        exitButton.setPreferredSize(new Dimension(120, 31));
+        exitButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        exitButton.addActionListener(e -> {
             System.exit(0);
         });
-        buttonPanel.add(exitB);
+        exitButton.addActionListener(this);
+        buttonPanel.add(exitButton);
 
         add(buttonPanel);
-
-        startB.addActionListener(this);
-        highscoreB.addActionListener(this);
-        exitB.addActionListener(this);
-
         setVisible(true);
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        new Menu_GUI();
     }
 
     public void startGame() {
@@ -93,49 +78,6 @@ public class Menu_GUI extends JPanel implements ActionListener {
         });
         this.setVisible(false);
         //this.dispose();
-    }
-
-    public static void main(String[] args) throws FileNotFoundException {
-        new Menu_GUI();
-    }
-
-    public static ArrayList readFile(String name) throws FileNotFoundException {
-
-        BufferedReader reader = null;
-
-        ArrayList<String[]> highscore = new ArrayList<String[]>();
-        try {
-            String str = null;
-            reader = new BufferedReader(new FileReader(name));
-            int i = 0;
-
-            String[] tempArray = new String[3];
-
-            while ((str = reader.readLine()) != null) {
-                //Falls index < 6, dann schreibe String in String tempArray
-                if (i < 2) {
-                    tempArray[i] = str;
-                }
-                //Falls index == 2, dann alle notwendigen Zeilen gelesen
-                if ((i == 2)) {
-                    //Index wird wieder auf 0 gesetzt
-                    i = 0;
-                    //befülltes String Array wird der ArrayListe angefügt
-                    highscore.add(tempArray);
-                    tempArray = new String[3];
-                }
-                //System.out.println(str);
-                //Falls String kein Inhalt besitzt, setze mit while-Schleife fort
-                if (str.isEmpty()) {
-                    continue;
-                }
-                i++;
-            }
-        } catch (final IOException e) {
-            System.out.println("Error reading file");
-            throw new RuntimeException("Error reading file: " + e.getMessage(), e);
-        }
-        return highscore;
     }
 
     @Override
